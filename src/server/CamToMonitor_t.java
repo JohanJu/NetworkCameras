6cb041e5f7784ef93277f9b7ce2f2c8bcfe98f75
Johan Ju <johanj93@hotmail.com>
2015-12-02 09:54:34 +0100

    add camToMon

---
+++ src/server/CamToMonitor_t.java
@@ -0,0 +1,29 @@
+package server;
+import se.lth.cs.eda040.fakecamera.*;
+public class CamToMonitor_t extends Thread{
+	Monitor mon;
+	public CamToMonitor_t(Monitor mon){
+		this.mon=mon;
+	}
+	public void run(){
+		AxisM3006V cam; 
+		cam = new AxisM3006V();
+		cam.init();
+		cam.setProxy("argus-1.student.lth.se", 55501);
+		cam.connect();
+		byte[] jpeg = new byte[AxisM3006V.IMAGE_BUFFER_SIZE+7];
+		int len;
+		byte mode = 0;
+		while(!isInterrupted()){
+			len = cam.getJPEG(jpeg, 7);
+			cam.getTime(jpeg, 0);
+			byte hi = (byte) (len / 255);
+			byte lo = (byte) (len % 255);
+			jpeg[4]=mode;
+			jpeg[5]=hi;
+			jpeg[6]=lo;
+			mon.setJpeg(jpeg);
+		}
+	}
+
+}
