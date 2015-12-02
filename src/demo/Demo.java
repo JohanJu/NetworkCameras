9d3c8167ed2cbc8da8d1a0496516db0e6f0828c4
Johan Ju <johanj93@hotmail.com>
2015-12-02 08:53:24 +0100

    init

--- src/demo/Demo.java
+++ src/demo/Demo.java
@@ -1,10 +1,39 @@
 package demo;
+import client.Client;
+import server.Server;
 
 public class Demo {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Server1 s1 = new Server1();
+		s1.start();
+		Server2 s2 = new Server2();
+		s2.start();
+		try {
+			Thread.currentThread().sleep(500);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		Client1 c = new Client1();
+		c.start();
+	}
+	
+	private static class Server1 extends Thread {
+		public void run() {
+			Server.main(new String[] {"6077"});
+		}
+	}
+	
+	private static class Server2 extends Thread {
+		public void run() {
+			Server.main(new String[] {"6078"});
+		}
+	}
+	
+	private static class Client1 extends Thread {
+		public void run() {
+			Client.main(new String[] {"localhost", "6077"});
+		}
 	}
 
 }
