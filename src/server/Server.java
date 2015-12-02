0d53e3e14c5423398da4d36719825a0ce56ca37e
Johan Ju <johanj93@hotmail.com>
2015-12-02 11:18:18 +0100

    change port

--- src/server/Server.java
+++ src/server/Server.java
@@ -1,16 +1,11 @@
 package server;
 
 public class Server {
-	static int port;
 	public static void main(String[] args) {
-		if(args.length==0){
-			port = 55501;
-		}else{
-			port = Integer.parseInt(args[0]);
-		}
+		System.out.println("main "+Integer.parseInt(args[0]));
 		Monitor mon = new Monitor();
 		Thread camToMonitor = new CamToMonitor_t(mon);
-		Thread fromClient = new FromClient_t(mon, port);
+		Thread fromClient = new FromClient_t(mon, Integer.parseInt(args[0]));
 		camToMonitor.start();
 		fromClient.start();	
 	}
