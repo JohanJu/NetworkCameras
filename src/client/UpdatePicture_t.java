23087cdd9446564ddeb764af5c75ebfee097701d
cbrandberg@me.com <cbrandberg@me.com>
2015-12-07 09:33:37 +0100

    display delay on GUI

--- src/client/UpdatePicture_t.java
+++ src/client/UpdatePicture_t.java
@@ -3,6 +3,11 @@ package client;
 public class UpdatePicture_t extends Thread {
 	Monitor monitor;
 	GUI gui;
+	long time1;
+	long time2;
+	boolean first1 = true;
+	boolean first2 = true;
+	
 	public UpdatePicture_t(Monitor monitor, GUI gui) {
 		this.monitor = monitor;
 		this.gui = gui;
@@ -12,12 +17,13 @@ public class UpdatePicture_t extends Thread {
 		while(true) {
 			PicData picture = monitor.getPicture();
 			int cam = monitor.getCamNbr(picture.port);
-			gui.updateIcon(picture.picture, cam);		//PORT??? ELLER CAM (SKA SKICKA 0/1)
+			gui.updateIcon(picture.picture, cam);
 			gui.updateTextOnTextField(gui.CAM_TRIGGERED_MOVIE, "" + monitor.camTriggeredMovieMode);
 			switch(picture.mode) {
 			case Monitor.AUTO:
 				if(cam == 0) {
 					gui.updateTextOnTextField(gui.MODE_CAM1, "auto");
+					
 				} else {
 					gui.updateTextOnTextField(gui.MODE_CAM2, "auto");
 				}
@@ -36,6 +42,22 @@ public class UpdatePicture_t extends Thread {
 					gui.updateTextOnTextField(gui.MODE_CAM2, "movie");
 				}
 				break;
+			}
+			// update delay
+			if(first1 && cam == 0) {
+				gui.updateTextOnTextField(gui.DELAY_CAMERA1, "" + 0);
+				time1 = picture.timeStamp;
+				first1 = false;
+			} else if(!first1 && cam == 0) {
+				gui.updateTextOnTextField(gui.DELAY_CAMERA1, "" + (picture.timeStamp - time1));
+				time1 = picture.timeStamp;
+			} else if(first2 && cam == 1) {
+				gui.updateTextOnTextField(gui.DELAY_CAMERA2, "" + 0);
+				time2 = picture.timeStamp;
+				first2 = false;
+			} else {
+				gui.updateTextOnTextField(gui.DELAY_CAMERA2, "" + (picture.timeStamp - time2));
+				time2 = picture.timeStamp;
 			}
 		}
 	}
