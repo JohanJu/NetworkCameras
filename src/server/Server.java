f651cd5407919fabb0229c8d8d479d0219d63248
Kristoffer Lundgren <kristoffer.lundgren@gmail.com>
2015-12-07 15:36:21 +0100

    Cleaned up Server.java, renamed _T to _t, removed JPEGHTTPServer.java.

--- src/server/Server.java
+++ src/server/Server.java
@@ -1,7 +1,5 @@
 package server;
 
-import server.JPEGHTTPServer;
-
 public class Server {
 	public static void main(String[] args) {
 		System.out.println("main "+Integer.parseInt(args[0]));
@@ -12,23 +10,5 @@ public class Server {
 		fromClient.start();
 		HTTP_t http = new HTTP_t(mon, Integer.parseInt(args[1]));
 		http.start();
-//		JHServer jhserver = new JHServer(mon, args[1]);
-//		jhserver.start();
-
 	}
-	
-//	private static class JHServer extends Thread {
-//		
-//		Monitor mon;
-//		String port;
-//		
-//		public JHServer (Monitor mon, String port) {
-//			this.mon = mon;
-//			this.port = port;
-//		}
-//		
-//		public void run() {
-//			JPEGHTTPServer.main(new String[] {port}, mon);
-//		}
-//	}
 }
